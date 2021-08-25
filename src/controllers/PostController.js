6133a205f0124e20bcf92e35ad7506b1a3ad03e3
qudtjs0753 <qudtjs0753@naver.com>
2021-08-26 00:50:41 +0900

    Feat: 이미지 업로드 기능 구현

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import * as Auth from '../../middleware/auth';
+import upload from '../../middleware/imageUpload';
 import * as PostService from '../services/PostService';
-
 const router = express.Router();
 
-router.post('/upload', PostService.writePost);
-router.post('/edit', PostService.editPost);
-router.post('/delete', PostService.deletePost);
-
+router.post('/upload', Auth.isLoggined, PostService.writePost);
+router.post('/edit', Auth.isLoggined, PostService.editPost);
+router.post('/delete', Auth.isLoggined, PostService.deletePost);
+router.post('/imageupload', upload.single('file'));
 export default router;
