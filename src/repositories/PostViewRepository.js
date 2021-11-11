4a5c4d9d131a8bbd4d8425191fdfbc07fc18f2f2
qudtjs0753 <qudtjs0753@naver.com>
2021-11-12 02:42:27 +0900

    Fix: Banner 오류 수정

--- src/repositories/PostViewRepository.js
+++ src/repositories/PostViewRepository.js
@@ -122,7 +122,7 @@ export const getFavoritePostRandomly = async user_id => {
 };
 export const getRandomPost = async user_id => {
   try {
-    return await prisma.$queryRaw(`SELECT * from post WHERE user_id=${user_id} order by rand() limit 1;`);
+    return await prisma.$queryRaw(`SELECT * FROM post WHERE post.user_id=${user_id} ORDER BY rand() limit 1;`);
   } catch (err) {
     console.error(err);
   }
--- src/services/BannerService.js
+++ src/services/BannerService.js
@@ -4,12 +4,14 @@ export const getBannerPost = async (req, res, next) => {
   try {
     const user = req.session.passport.user;
     const favoritePost = await PostViewRepository.getFavoritePostRandomly(user.id);
-    if (!favoritePost) {
+    if (!favoritePost[0]) {
       const randomPost = await PostViewRepository.getRandomPost(user.id);
-      return res.status(200).send(randomPost);
+      const post = await PostViewRepository.getPost(randomPost[0].id);
+      return res.status(200).send(post);
       //여기 수정 필요
     } else {
-      return res.status(200).send(favoritePost);
+      const post = await PostViewRepository.getPost(favoritePost[0].id);
+      return res.status(200).send(post);
     }
   } catch (err) {
     console.error(err);
