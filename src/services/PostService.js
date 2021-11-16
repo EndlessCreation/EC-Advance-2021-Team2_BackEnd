ca8f414479bbd971f09c4877c1beeb5fde47f9ea
qudtjs0753 <qudtjs0753@naver.com>
2021-11-17 01:02:25 +0900

    Refactor: Algolia와 관련된 post, image update 구조 수정

--- src/services/PostService.js
+++ src/services/PostService.js
@@ -1,7 +1,7 @@
 import algolia from '../../configs/algolia';
-import { deleteImageFromDB, deleteImageFromServer, updateImageToDB } from '../../utils/ImageUtil';
+import * as ImageUtil from '../../utils/ImageUtil';
+import { deleteImageFromServer } from '../../utils/ImageUtil';
 import * as PostUtil from '../../utils/PostUitl';
-import * as ImageRepository from '../repositories/ImageRepository';
 import * as PostRepository from '../repositories/PostRepository';
 export const writePost = async (req, res, next) => {
   try {
@@ -9,31 +9,13 @@ export const writePost = async (req, res, next) => {
     req.body.tag_id = parseInt(req.body.tag_id);
     req.body.keyword_id = parseInt(req.body.keyword_id);
     const post = await PostUtil.createPost(req.body, req.session.passport.user.id);
-    let image;
-    //게시글 업로드 오류발생시.
-    if (!post) {
-      return res.status(400).send('게시글을 올리는 도중 오류가 발생하였습니다.');
-    }
 
-    if (!req.file) {
-      //사진을 첨부하지 않았을 시.
-      post.objectID = post.id;
-      post.image = null;
-      await algolia.saveObject(post, { autoGenerateObjectIDIfNotExist: true });
-      return res.status(200).send(post);
-    } else {
-      //사진 첨부시.
-      image = await ImageRepository.createImage(post.id, req.file.filename);
-    }
-    //image, post 업로드 뒤에 오류 체크.
-    if (!image) {
-      return res.status(400).send('이미지 업로드 실패. 게시글 수정을 통해 다시 올려주세요');
-    } else {
-      post.objectID = post.id;
-      post.image = image;
-      await algolia.saveObject(post, { autoGenerateObjectIDIfNotExist: true });
-      return res.status(200).send(image);
-    }
+    const isConnected = await ImageUtil.createImageWithServerAndAlgolia(req.file, post);
+
+    //게시글 업로드 오류발생시.
+    if (!post || !isConnected) return res.status(400).send('게시글을 올리는 도중 오류가 발생하였습니다.');
+
+    return res.status(200).send(post);
   } catch (err) {
     console.error(err);
     next(err);
@@ -53,42 +35,15 @@ export const editPost = async (req, res, next) => {
     //없는 포스트에 요청시 오류.
     if (toEdit === null || toEdit === undefined) return res.status(400).send('이미 삭제된 게시글이거나 없는 게시글입니다.');
     //기존이미지삭제
-    deleteImageFromServer(toEdit.image);
-    //수정했을때 이미지가 있는 경우
-    if (req.file) {
-      const post = await PostUtil.updatePost(req.body, req.session.passport.user.id);
-      if (!post) res.status(400).send('게시글 수정 도중 문제가 발생하였습니다.');
-      const editedImage = await updateImageToDB(toEdit.image, { path: req.file.filename, post_id: req.body.post_id });
-      await algolia.partialUpdateObjects([
-        {
-          content: post.content,
-          updateAt: post.updateAt,
-          tag_id: post.tag_id,
-          keyword_id: post.keyword_id,
-          objectID: `${post.id}`,
-          image: editedImage,
-        },
-      ]);
-      return res.status(200).send(post);
-    } else {
-      //이미지가 없는 경우
-      const post = await PostUtil.updatePost(req.body, req.session.passport.user.id);
-      const deletedImage = await deleteImageFromDB(toEdit.image);
-      if (!deletedImage) {
-        return res.status(400).send('게시글 수정 도중 문제가 발생하였습니다.');
-      }
-      await algolia.partialUpdateObjects([
-        {
-          content: post.content,
-          updateAt: post.updateAt,
-          tag_id: post.tag_id,
-          keyword_id: post.keyword_id,
-          objectID: `${post.id}`,
-          image: null,
-        },
-      ]);
-      return res.status(200).send(post);
-    }
+    console.log(toEdit);
+    const isDeleted = deleteImageFromServer(toEdit.image);
+    const post = await PostUtil.updatePost(req.body, req.session.passport.user.id);
+    const isUpdated = await ImageUtil.updateImageWithServerAndAlgolia(req.file, post);
+
+    if (!post || !isUpdated || !isDeleted) return res.status(400).send('게시글 수정 도중 문제가 발생하였습니다.');
+
+    return res.status(200).send(post);
+
     //여긴 나중에 req.params.postid로 바꿀것.
   } catch (err) {
     console.error(err);
--- utils/ImageUtil.js
+++ utils/ImageUtil.js
@@ -1,18 +1,22 @@
 import fs from 'fs';
 import path from 'path';
+import algolia from '../configs/algolia';
 import * as ImageRepository from '../src/repositories/ImageRepository';
+import { createImage } from '../src/repositories/ImageRepository';
 
 export const deleteImageFromServer = async image => {
   try {
-    if (image !== null && image !== undefined) fs.unlinkSync(path.join(__dirname, `../images/${image.path}`));
+    if (image !== null) fs.unlinkSync(path.join(__dirname, `../images/${image.path}`));
+    return true;
   } catch (err) {
     console.error(err);
+    return false;
   }
 };
 
 export const deleteImageFromDB = async image => {
   try {
-    if (image) return await ImageRepository.deleteImage(image.id);
+    if (image !== undefined) return await ImageRepository.deleteImage(image.id);
     else return true;
   } catch (err) {
     console.error(err);
@@ -21,11 +25,44 @@ export const deleteImageFromDB = async image => {
 
 export const updateImageToDB = async (image, data) => {
   try {
-    console.log(image);
-    console.log(data);
-    if (image) return await ImageRepository.updateImage(image.id, data);
-    else return await ImageRepository.createImage(data.post_id, data.path);
+    if (image !== undefined) return await ImageRepository.updateImage(image.id, data);
+    return await ImageRepository.createImage(data.post_id, data.path);
   } catch (err) {
     console.error(err);
+  }
+};
+
+export const createImageWithServerAndAlgolia = async (imageFile, post) => {
+  try {
+    if (imageFile === undefined) {
+      post.image = null;
+    } else {
+      const image = await createImage(post.id, imageFile.filename);
+      post.image = image;
+    }
+    post.objectID = post.id;
+    await algolia.saveObject(post, { autoGenerateObjectIDIfNotExist: true });
+    return true;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+};
+
+export const updateImageWithServerAndAlgolia = async (imageFile, post) => {
+  try {
+    if (imageFile === undefined) {
+      await deleteImageFromDB(post.image);
+      post.image = null;
+    } else {
+      const editedImage = await updateImageToDB(post.image, { path: imageFile.filename, post_id: post.id });
+      post.image = editedImage;
+    }
+    post.objectID = post.id;
+    await algolia.partialUpdateObject(post);
+    return true;
+  } catch (err) {
+    console.error(err);
+    return false;
   }
 };
